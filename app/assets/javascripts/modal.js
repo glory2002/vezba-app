a8404c717cd904923379203306bab28f9bcc69db
glory <glory.angelova@gmail.com>
2017-01-24 10:34:51 +0100

    pop up js modal box

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,38 +1,14 @@
-window.onload = function(){
+$(document).on("turbolinks:load", function() {
+  $("#myBtn").on("click", function(e){
+    e.preventDefault();
+    $("#myModal").show();
+  });
 
-  var modal = document.getElementById('myModal');
+  $(".close").on("click", function(){
+    $("#myModal").hide();
+  });
 
-  var btn = document.getElementById("myBtn");
-
-  var span = document.getElementsByClassName("close")[0];
-
-  btn.onclick = function(event) {
-    event.preventDefault();
-    modal.style.display = "block"
-  }
-
-  span.onclick = function() {
-      modal.style.display = "none";
-  }
-
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  }
-
-  // var myIndex = 0;
-  // carousel();
-  //
-  // function carousel() {
-  //     var i;
-  //     var x = document.getElementsByClassName("mySlides");
-  //     for (i = 0; i < x.length; i++) {
-  //        x[i].style.display = "none";
-  //     }
-  //     myIndex++;
-  //     if (myIndex > x.length) {myIndex = 1}
-  //     x[myIndex-1].style.display = "block";
-  //     setTimeout(carousel, 2000);
-  // }
-};
+  $("#myModal").on("click", function(){
+    $("#myModal").hide();
+  });
+})
